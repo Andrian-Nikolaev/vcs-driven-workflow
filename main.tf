55a142c565180cc6131f700f592e186f2e957d0f
Andrian <andrian.svezhenovnikolaev@hashicorp.com>
2025-06-18 18:55:28 +0300

    removing cloud block

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
-terraform {
-  cloud {
-    organization = "onboarding-terraform-andrian"
-    workspaces {
-      name = "vcs-workspace"
-    }
-  }
-}
+# terraform {
+#   cloud {
+#     organization = "onboarding-terraform-andrian"
+#     workspaces {
+#       name = "vcs-workspace"
+#     }
+#   }
+# }
 
 resource "null_resource" "testing" {
   
